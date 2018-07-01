61ab09ff23e23732fb623f35698435ab706f0848
Darri Steinn Konradsson <darri15@hotmail.com>
2018-07-01 12:46:21 +0000

    Add base class for commands

---
+++ todo/commands/base.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+
+
+class Command(ABC):
+    arguments = tuple()
+
+    def is_valid_argument(self, arg):
+        return arg in self.arguments
+
+    def __init__(self, service):
+        self.service = service
+
+    def _get_todo_or_raise(self, id):
+        todo = self.service.todo.get(id)
+        if todo is None:
+            raise Exception('not found')
+
+        return todo
+
+    def _get_group_or_raise(self, name):
+        group = self.service.group.get(name)
+        if group is None and name != 'global':
+            raise Exception('not found')
+
+        return group
+
+    @abstractmethod
+    def run(self, *args, **kwargs):
+        pass
