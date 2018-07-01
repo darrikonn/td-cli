5f8ea9159f736375ef1075c305611440ed8f9acf
Darri Steinn Konradsson <darri15@hotmail.com>
2018-07-01 12:42:37 +0000

    Split up services

---
+++ todo/services/__init__.py
@@ -0,0 +1,50 @@
+import configparser
+import sqlite3
+from urllib.request import pathname2url
+
+from .group import GroupService
+from .todo import TodoService
+
+
+class Service:
+    __slots__ = ('connection', 'cursor')
+
+    service_dict = {
+        'todo': TodoService,
+        'group': GroupService,
+    }
+
+    def __init__(self):
+        config = configparser.SafeConfigParser({'name': 'todo.db'})
+        config.read('todo/todo.cfg')  # TODO: get full path and use .rc
+
+        database_name = config.get('Database', 'name')
+        db_uri = 'file:{}'.format(pathname2url(database_name))
+
+        try:
+            self.connection = sqlite3.connect('{}?mode=rw'.format(db_uri), uri=True)
+            self.cursor = self.connection.cursor()
+        except sqlite3.OperationalError:
+            self._initialise_database(db_uri)
+        self._link_services()
+
+        self.cursor.execute("PRAGMA foreign_keys = ON")
+
+    def _initialise_database(self, db_uri):
+        self.connection = sqlite3.connect(db_uri, uri=True)
+        self.cursor = self.connection.cursor()
+        for _, cls in self.service_dict.items():
+            cls.initialise_table(self)
+        self.connection.commit()
+
+    def _link_services(self):
+        for prop, service in self.service_dict.items():
+            setattr(Service, prop, service(self.connection, self.cursor))
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type is not None:
+            return False
+        self.connection.close()
