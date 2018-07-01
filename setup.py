8615ba480122c4691115ed4636c5a4814ba7507e
Darri Steinn Konradsson <darri15@hotmail.com>
2018-07-01 12:41:18 +0000

    Td-cli should only be for python 3^

--- setup.py
+++ setup.py
@@ -10,19 +10,16 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='todo',
+    name='td-cli',
     version='0.0.1',
     description='A todo command line tool',
-    url='https://github.com/darrikonn/todo',
+    url='https://github.com/darrikonn/td-cli',
     author='Darri Steinn Konn Konradsson',
     author_email='darrikonn@gmail.com',
     license='MIT',
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Intended Audience :: Command line enthusiasts',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
@@ -30,14 +27,15 @@ setup(
     ],
     keywords='todo commandline',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, <4',
-    install_requires=['click'],
+    python_requires='!=3.0.*, !=3.1.*, !=3.2.*, <4',
+    install_requires=[],
     entry_points={
         'console_scripts': [
+            # TODO
             'todo=todo:main',
         ],
     },
     project_urls={
-        'Source': 'https://github.com/darrikonn/todo',
+        'Source': 'https://github.com/darrikonn/td-cli',
     },
 )
