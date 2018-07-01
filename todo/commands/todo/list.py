e738b8046b8de6276a22323376d3e023a0db2369
Darri Steinn Konradsson <darri15@hotmail.com>
2018-07-01 15:34:47 +0000

    Print list todo footer

--- todo/commands/todo/list.py
+++ todo/commands/todo/list.py
@@ -24,17 +24,26 @@ class List(Command):
         self._print_header(active_group[0] or "global", subsequent_indent)
 
         for todo in todos:
-            prefix = '\033[34m{}  \033[37m'.format(todo[0])
-            wrapper = textwrap.TextWrapper(
-                initial_indent=prefix,
-                width=cols,
-                subsequent_indent=' ' * 8,
-            )
+            prefix = "{bold}{id}{reset}  ".format(id=todo[0], bold=Style.BOLD, reset=Style.RESET_ALL)
+            wrapper = textwrap.TextWrapper(initial_indent=prefix, width=cols, subsequent_indent=subsequent_indent)
             print(wrapper.fill(todo[1]))
+
+        self._print_footer(len(completed_todos), len(uncompleted_todos), len(todos))
 
     def _print_header(self, name, subsequent_indent):
         print(
             "{subsequent_indent}{bold}{blue}{name}{reset}\n".format(
                 name=name, subsequent_indent=subsequent_indent, blue=Fore.BLUE, bold=Style.BOLD, reset=Style.RESET_ALL
+            )
+        )
+
+    def _print_footer(self, completed, uncompleted, listed):
+        print(
+            "{prefix}{info}{items} items: {completed} completed, {uncompleted} uncompleted".format(
+                prefix="\n" if listed > 0 else "",
+                items=completed + uncompleted,
+                completed=completed,
+                uncompleted=uncompleted,
+                info=Fore.INFO,
             )
         )
