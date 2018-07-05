76eac1c73274a16c716f0678e31792b141c5dd1b
Darri Steinn Konradsson <darri15@hotmail.com>
2018-07-05 13:58:06 +0000

    Add output renderer with text wrapper

---
+++ todo/renderers/render_output_with_textwrap.py
@@ -0,0 +1,22 @@
+import textwrap
+from os import get_terminal_size
+
+from .base import Render
+
+
+class RenderOutputWithTextwrap(Render):
+    def __init__(self, prefix, text_to_wrap):
+        self.prefix = prefix
+        self.text_to_wrap = text_to_wrap
+
+    def render(self, **kwargs):
+        kwargs.setdefault("subsequent_indent", " " * 8)
+
+        cols, _ = get_terminal_size()
+
+        wrapper = textwrap.TextWrapper(
+            initial_indent=self._format(self.prefix, **kwargs),
+            width=cols,
+            subsequent_indent=kwargs.get("subsequent_indent"),
+        )
+        print(wrapper.fill(self._format(self.text_to_wrap, **kwargs)))
