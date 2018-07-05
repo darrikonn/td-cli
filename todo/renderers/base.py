1a6511e1ed1f60970d9321adcfa17f3662c29770
Darri Steinn Konradsson <darri15@hotmail.com>
2018-07-05 13:56:57 +0000

    Add base renderer

---
+++ todo/renderers/base.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+from .styles import Fore, Style
+
+
+class Render(ABC):
+    def _format(self, text, **kwargs):
+        return text.format(
+            red=Fore.RED,
+            green=Fore.GREEN,
+            blue=Fore.BLUE,
+            white=Fore.WHITE,
+            grey=Fore.GREY,
+
+            bold=Style.BOLD,
+            normal=Style.NORMAL,
+            reset=Style.RESET_ALL,
+
+            **kwargs
+        )
+
+    @abstractmethod
+    def render(self, *args, **kwargs):
+        pass
