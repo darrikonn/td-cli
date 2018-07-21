763d51ed223d6d5ccc0f0c49aafc8e88898fb20e
Darri Steinn Konradsson <darri15@hotmail.com>
2018-07-21 11:51:51 +0000

    Parse completed/uncompleted with interactive

--- todo/parsers/todos.py
+++ todo/parsers/todos.py
@@ -8,9 +8,14 @@ class TodosParser(BaseParser):
     def _normalize(self, parsed_args):
         if len(sys.argv[1:]) == 0:
             setattr(parsed_args, 'list_todos', STATES.UNCOMPLETED)
+        elif parsed_args.interactive:
+            setattr(parsed_args, 'interactive', parsed_args.list_todos or True)
+            setattr(parsed_args, 'list_todos', None)
         return parsed_args
 
     def _add_arguments(self):
+        self.parser.add_argument('--interactive', '-i', action='store_true')
+
         parser_group = self.parser.add_mutually_exclusive_group()
         parser_group.add_argument(
             '--completed',
@@ -19,5 +24,11 @@ class TodosParser(BaseParser):
             nargs=0,
             action=set_value(STATES.COMPLETED),
         )
+        parser_group.add_argument(
+            '--uncompleted',
+            '-u',
+            dest='list_todos',
+            nargs=0,
+            action=set_value(STATES.UNCOMPLETED),
+        )
         parser_group.add_argument('--add', '-a', dest='add_todo', action='store')
-        parser_group.add_argument('--interactive', '-i', action='store_true')
