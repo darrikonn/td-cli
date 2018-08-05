47f097b8c30b54822d08cc156cd4d62a9158ec29
Darri Steinn Konradsson <darri15@hotmail.com>
2018-08-05 23:23:19 +0000

    Rename `use` to `preset`

--- todo/commands/group/preset.py
+++ todo/commands/group/preset.py
@@ -2,10 +2,10 @@ from todo.commands.base import Command
 from todo.renderers import RenderOutput
 
 
-class Use(Command):
+class Preset(Command):
     def run(self, name):
         group = self._get_group_or_raise(name)
 
         self.service.group.use(group[0])
 
-        RenderOutput("Using group {blue}{group_name}").render(group_name=group[0] or "global")
+        RenderOutput("Set {blue}{group_name}{reset} as default").render(group_name=group[0] or "global")
