3bb6cc63819003ede78a6f474b3815664924c0d3
Darri Steinn Konradsson <darri15@hotmail.com>
2018-09-20 17:53:24 +0000

    Remove default state

--- todo/settings.py
+++ todo/settings.py
@@ -2,11 +2,9 @@ import configparser
 import os
 from functools import lru_cache
 
-from todo.constants import STATES
-
 CONFIG_FILE_NAME = "~/.td.cfg"
 CONFIG_SECTION = "settings"
-DEFAULT_CONFIG = {"database_name": "todo", "editor": "vi", "default_state": None}
+DEFAULT_CONFIG = {"database_name": "todo", "editor": "vi"}
 
 
 @lru_cache()
@@ -18,13 +16,6 @@ def _get_config():
         config.read(config_file)
         if config.has_section(CONFIG_SECTION):
             user_settings = dict(config.items(CONFIG_SECTION))
-            if "default_state" in user_settings:
-                if user_settings["default_state"] == STATES.COMPLETED:
-                    user_settings["default_state"] = True
-                elif user_settings["default_state"] == STATES.UNCOMPLETED:
-                    user_settings["default_state"] = False
-                else:
-                    user_settings["default_state"] = None
             settings.update(user_settings)
     return settings
 
