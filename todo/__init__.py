19c876f927f13b23e26916bf40728efe765ce013
Darri Steinn Konradsson <darri15@hotmail.com>
2018-10-01 14:01:28 +0000

    Enforce python 3

--- todo/__init__.py
+++ todo/__init__.py
@@ -2,13 +2,16 @@
 
 import sys
 
-from todo.commands import Commands
-from todo.exceptions import TodoException
-from todo.parser import Parser
-from todo.renderers import RenderError
+if sys.version_info < (3,):
+    sys.exit("Sorry, td-cli only supports Python >= 3")
 
 
 def main(args=None):
+    from todo.commands import Commands
+    from todo.exceptions import TodoException
+    from todo.parser import Parser
+    from todo.renderers import RenderError
+
     if args is None:
         args = sys.argv[1:]
     verbose = any(verbose in args for verbose in ("--verbose", "-v"))
