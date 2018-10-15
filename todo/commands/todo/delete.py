b685aafdb98400c678f91ec93e0c7efcbb6a7111
Darri Steinn Konradsson <darri15@hotmail.com>
2018-10-15 14:44:42 +0000

    Render output for aborting

--- todo/commands/todo/delete.py
+++ todo/commands/todo/delete.py
@@ -2,7 +2,7 @@ from sqlite3 import Error
 
 from todo.commands.base import Command
 from todo.exceptions import TodoException
-from todo.renderers import RenderInput, RenderOutputWithTextwrap
+from todo.renderers import RenderInput, RenderOutput, RenderOutputWithTextwrap
 
 
 class Delete(Command):
@@ -11,10 +11,10 @@ class Delete(Command):
             todo = self._get_todo_or_raise(args.id)
             if not args.skip_prompt:
                 choice = RenderInput(
-                    "[?] Are you sure you want to delete todo {bold}{todo_id}{normal}? [Y|n]"
+                    "[?] Are you sure you want to delete todo {bold}{todo_id}{normal}? [Y|n] "
                 ).render(todo_id=todo[0])
                 if choice not in ("", "y", "ye", "yes"):
-                    raise Exception("Abort!")
+                    return RenderOutput("Abort!").render()
             self.service.todo.delete(todo[0])
 
             RenderOutputWithTextwrap(
