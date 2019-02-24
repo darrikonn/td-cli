e333098104d4c3ab9b835f958a834cdf41f91b1a
Darri Steinn Konn Konradsson <darrikonn@gmail.com>
2019-02-24 22:06:21 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='td-cli',
-    version='0.1.17',
+    version='1.0.0',
     description='A todo command line manager',
     long_description=long_description,
     long_description_content_type='text/markdown',
