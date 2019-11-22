56e7ae6670e3fe362c80a2615126666ec4d4dc87
Darri Steinn Konradsson <darrikonn@gmail.com>
2019-11-22 12:04:30 +0000

    Update patch version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="td-cli",
-    version="1.2.4",
+    version="1.2.5",
     description="A todo command line manager",
     long_description=long_description,
     long_description_content_type="text/markdown",
